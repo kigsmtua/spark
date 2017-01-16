d74d787eda1edb0ba8903c926b804011f3558a25
John Kiragu <kiragu@adalavice.co.ke>
2017-01-16 13:58:20 +0300

    Added german implementation to use in background jobs

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','session');
+$autoload['libraries'] = array('database','session','lib_gearman');
 
 /*
 | -------------------------------------------------------------------
@@ -103,7 +103,7 @@ $autoload['helper'] = array('url','file');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('gearman');
 
 /*
 | -------------------------------------------------------------------
