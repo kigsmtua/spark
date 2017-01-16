5f737272041fd368f4ce8b6ea229d69781f04c7a
John Kiragu <kiragu@adalavice.co.ke>
2017-01-16 10:42:04 +0300

    Added logging functionality with log4php

--- application/models/User.php
+++ application/models/User.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends MY_Model
 {
-
+  ###Lets maske the same parent
   function __construct()
   {
      parent::__construct();
