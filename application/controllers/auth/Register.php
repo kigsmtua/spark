83171afb2ba97813579bb9931a67a0046611a493
John Kiragu <kiragu@adalavice.co.ke>
2017-02-17 16:19:19 +0300

    Changes made to authentication module

--- application/controllers/auth/Register.php
+++ application/controllers/auth/Register.php
@@ -2,20 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * 
+ * Do all the registeration action based on this work
  */
-class Login extends MY_Controller
+class Register extends MY_Controller
 {
   
-  function __construct(argument)
+  function __construct()
   {
      parent::__construct();
   }
 
-  
   public function index(){
-  
-     #Automatically login the user after registeration
+   
+      $data = array();
+      $data['mainContent'] = 'auth/register';
+      $this->load->view('route',$data);
+  }
+
+  public function store(){
+    
 
   }
 
