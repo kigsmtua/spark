46f54f5c4e982bc381b0da7b25490068b210f4e6
John Kiragu <kiragu@adalavice.co.ke>
2017-02-17 15:43:26 +0300

    Added Billing And Subscription Model

--- application/controllers/auth/Login.php
+++ application/controllers/auth/Login.php
@@ -1,17 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
-* 
-*/
+
+
 class Login extends MY_Controller
 {
   
-  function __construct(argument)
+
+  function __construct()
   {
      parent::__construct();
   }
-
   
   public function index(){
     $this->load->library('authentication');
@@ -19,10 +18,10 @@ class Login extends MY_Controller
         $this->require_min_level(1);
     $this->setup_login_form();
     $data = array();
-    $data['mainContent'] = 'site/login';
+    $data['mainContent'] = 'auth/login';
     $this->load->view('route',$data);
   }
-
+  
   public function destroy(){
     $this->load->library('authentication');
     $this->authentication->logout();
